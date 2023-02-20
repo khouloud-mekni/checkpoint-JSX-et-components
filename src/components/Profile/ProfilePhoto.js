11d3d246ad4f99ef2448d0142786644b94ec35e2
khouloudmekni <khouloudwerhani122@gmail.com>
2023-02-20 23:24:49 +0100

    jsx and components

---
+++ src/components/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from "react";
+import hk from '../../assets/hkcat.jpg'
+const ProfilePhoto = () => {
+  return (
+    <div>
+<img src={hk} alt="my img"/>
+    </div>
+  )
+}
+
+export default ProfilePhoto
